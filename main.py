26973de0fa16221ad05c24475cd53856ff989808
shizhe14 <zyeric.shi@mail.utoronto.ca>
2024-10-25 23:16:07 -0400

    added pre-processing screen with integrated interactive 3d mayavi plot from localization dataset

--- main.py
+++ main.py
@@ -1,18 +1,64 @@
 import sys, os
 import numpy as np
+import pandas as pd
 import math
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QTableWidgetItem, QMessageBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QTableWidgetItem, QMessageBox, QWidget, QVBoxLayout
 from PyQt6 import uic
 from PyQt6.QtCore import Qt
+from pyface.qt import QtGui, QtCore
 
 from MixtureModelAlgorithm import EM1, EM2, EM3  # Import from the original script
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
+from mayavi import mlab
+from traits.api import HasTraits, Instance, on_trait_change
+from traitsui.api import View, Item
+from mayavi.core.ui.api import MayaviScene, MlabSceneModel, SceneEditor
+from mayavi.mlab import contour3d
+
 
 print("Initializing UI")
+
+## create Mayavi Widget and show
+
+class Visualization(HasTraits):
+    scene =  Instance(MlabSceneModel, ())
+
+    @on_trait_change('scene.activated')
+    def update_plot(self):
+        df = window.localization_data
+        ## PLot to Show  
+        # Extract X, Y, and time frame positions
+        x_positions = df.iloc[:, 0]
+        y_positions = df.iloc[:, 1]
+        time_frame = df.iloc[:, 2]
+
+        # Combine X, Y, and time frame into a single array for DBSCAN
+        positions = np.vstack((x_positions, y_positions, time_frame)).T      
+        # Assuming positions is your combined 3D data (x, y, time)
+        mlab.points3d(positions[:, 0], positions[:, 1], positions[:, 2], 
+                    scale_factor=40  # Reduce data point size by half
+                    )  # Adjust colormap as needed
+
+    view = View(Item('scene', editor=SceneEditor(scene_class=MayaviScene),
+                        height=250, width=300, show_label=False),
+                resizable=True )
+
+class MayaviQWidget(QtGui.QWidget):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        layout = QtGui.QVBoxLayout(self)
+        layout.setContentsMargins(0,0,0,0)
+        layout.setSpacing(0)
+        self.visualization = Visualization()
+
+        self.ui = self.visualization.edit_traits(parent=self,
+                                                    kind='subpanel').control
+        layout.addWidget(self.ui)
+        self.ui.setParent(self)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -26,10 +72,12 @@ class MainWindow(QMainWindow):
         self.inputLambda.setText("0")
         self.inputTheta.setText("1")
 
-        # self.paramWidget.setStyleSheet("border: 1px solid red")
+        self.preprocessing_clicked(None)
 
-        self.data = None  # Placeholder for the loaded data
-        self.data_imported = False
+        self.blinking_data = None
+        self.blinking_data_imported = False
+        self.localization_data = None
+        self.localization_data_imported = False
         self.lab_ineff = False
 
         self.replicates = 1
@@ -41,11 +89,15 @@ class MainWindow(QMainWindow):
         # Connect the buttons to their respective functions
         self.runButton.clicked.connect(self.run_replicates)
         # Connect Menu items to their functions
-        self.actionLoadFile.triggered.connect(self.load_file)
+        self.actionLoadBlinking.triggered.connect(self.load_blinking)
+        self.actionLoadLocalization.triggered.connect(self.load_localization)
         self.actionGraph_Dataset.triggered.connect(self.plot_dataset)
         self.radioEM1.clicked.connect(self.set_default_pi)
         self.radioEM2.clicked.connect(self.set_default_pi)
         self.radioEM3.clicked.connect(self.set_default_pi)
+
+        self.preprocessingSwitch.mousePressEvent = self.preprocessing_clicked
+        self.stoichiometrySwitch.mousePressEvent = self.stoichiometry_clicked
 
     def initialize_stoichiometry_graph(self):
         plt.rc('font', family='Calibri')
@@ -72,22 +124,58 @@ class MainWindow(QMainWindow):
         base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
         return os.path.join(base_path, relative_path)
 
-    def load_file(self):
+    def preprocessing_clicked(self, event):
+        self.preprocessingSwitch.setStyleSheet("color: black;")
+        self.stoichiometrySwitch.setStyleSheet("color: gray;")
+        self.confStack.setCurrentIndex(1)
+        self.resultStack.setCurrentIndex(1)
+        self.graphStack.setCurrentIndex(1)
+
+    def stoichiometry_clicked(self, event):
+        self.preprocessingSwitch.setStyleSheet("color: gray;")
+        self.stoichiometrySwitch.setStyleSheet("color: black;")
+        self.confStack.setCurrentIndex(0)
+        self.resultStack.setCurrentIndex(0)
+        self.graphStack.setCurrentIndex(0)
+
+    def load_blinking(self):
         # Open a file dialog to select the CSV file
         file_dialog = QFileDialog()
         file_path, _ = file_dialog.getOpenFileName(self, "Open Data File", "", "CSV Files (*.csv)")
         
         if file_path:
             # Display the file path in the UI
-            self.filePathLabel.setText(f"Loaded file: {file_path}")
+            self.blinkingFilePathLabel.setText(f"Loaded Blinking Dataset: {file_path}")
 
             # Load the CSV file using numpy
-            self.data = np.genfromtxt(file_path, delimiter=",")
-            self.data_imported = True
-        elif self.data_imported == True:
+            self.blinking_data = np.genfromtxt(file_path, delimiter=",")
+            self.blinking_data_imported = True
+            self.stoichiometry_clicked(None)
+        elif self.blinking_data_imported == True:
             pass
         else:
-            self.filePathLabel.setText("No file loaded")
+            self.blinkingFilePathLabel.setText("No file loaded")
+
+    def load_localization(self):
+        # Open a file dialog to select the CSV file
+        file_dialog = QFileDialog()
+        file_path, _ = file_dialog.getOpenFileName(self, "Open Data File", "", "Text Files (*.txt)")
+        
+        if file_path:
+            # Display the file path in the UI
+            self.localizationFilePathLabel.setText(f"Loaded Localization Dataset: {file_path}")
+
+            # Load the CSV file using numpy
+            self.localization_data = pd.read_csv(file_path, delimiter=' ', header=1)
+
+            self.localization_data_imported = True
+            self.preprocessing_clicked(None)
+            mayavi_widget = MayaviQWidget(self)
+            self.localizationGraphSpace.addWidget(mayavi_widget)
+        elif self.localization_data_imported == True:
+            pass
+        else:
+            self.localizationFilePathLabel.setText("No file loaded")
 
     def set_default_pi(self):
         if self.radioEM1.isChecked():
@@ -99,7 +187,7 @@ class MainWindow(QMainWindow):
     
     def run_replicates(self):
         # Ensure data is loaded
-        if self.data is None:
+        if self.blinking_data is None:
             # self.resultDisplay.setText("Please load a data file before running the algorithm")
             self.show_popup("Missing data", "Please load a data file before running the algorithm")
             return
@@ -226,7 +314,7 @@ class MainWindow(QMainWindow):
 
     def plot_dataset(self):
         
-        if not self.data_imported:
+        if not self.blinking_data_imported:
             # self.resultDisplay.setText("Please import the data file before plotting")
             self.show_popup("Missing data", "Please import the data file before plotting")
 
@@ -235,7 +323,7 @@ class MainWindow(QMainWindow):
         self.ax.clear()
 
         # Plot the data
-        self.ax.plot(sorted(self.data))
+        self.ax.plot(sorted(self.blinking_data))
         # Add labels and a title
         self.ax.set_xlabel("Dye (Sorted)")
         self.ax.set_ylabel("Number of Blinks")
@@ -282,8 +370,8 @@ class MainWindow(QMainWindow):
 
 
     def bootstrap_dataset(self, replicates, size_fraction):
-        print(math.floor(len(self.data)*size_fraction))
-        return [np.random.choice(self.data, size=math.floor(len(self.data)*size_fraction), replace=True) for _ in range(replicates)]
+        print(math.floor(len(self.blinking_data)*size_fraction))
+        return [np.random.choice(self.blinking_data, size=math.floor(len(self.blinking_data)*size_fraction), replace=True) for _ in range(replicates)]
 
 
 if __name__ == "__main__":
